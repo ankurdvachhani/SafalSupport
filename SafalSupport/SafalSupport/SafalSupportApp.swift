4fd58c2d3cac82a1ca55255389c58a3d171cd7cd
ankurdvachhani <ankur.d.vachhani@safalvir.com>
2025-10-17 14:22:04 +0530

    isseu fixed

---
+++ SafalSupport/SafalSupport/SafalSupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafalSupportApp.swift
+//  SafalSupport
+//
+//  Created by Apple on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SafalSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
